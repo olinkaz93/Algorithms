11ad0e2ad3da982dc0f5becdd98e05aa2f83a250
Ola Zieminska <oziemins@cisco.com>
2021-04-18 18:22:02 +0200

    leet1051

---
+++ Interview_Examples/Leetcode/1051_HeightChecker.py
@@ -0,0 +1,64 @@
+"""
+A school is trying to take an annual photo
+of all the students.
+
+The students are asked to stand in a single
+file line in non-decreasing order by height.
+Let this ordering be represented by the integer
+ array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights
+representing the current order that the students
+are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+Example 1:
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation:
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+
+Example 2:
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+
+Example 3:
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+
+"""
+
+def heightChecker(heights):
+    sorted_hegihts = sorted(heights)
+    unpaired_indeces = []
+    for i in range (len(heights)):
+        if heights[i] != sorted_hegihts[i]:
+            unpaired_indeces.append(i)
+
+    if len(unpaired_indeces) == 0:
+        return True
+    else:
+        return unpaired_indeces
+
+
+
+if __name__ == "__main__":
+    heights = [1, 2, 3, 4, 5]
+    heightChecker(heights)
+
+    """You are given an integer array heights
+    representing the current order that the students
+    are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+    Return the number of indices where heights[i] != expected[i]."""
