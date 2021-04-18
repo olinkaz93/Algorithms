1206e70a1defa05d251cad0fcdcafc6255ef3e8d
Ola Zieminska <oziemins@cisco.com>
2021-04-18 18:59:56 +0200

    53_leetcode_TBC

--- Interview_Examples/Leetcode/53_MaximumSubarray.py
+++ Interview_Examples/Leetcode/53_MaximumSubarray.py
@@ -4,7 +4,6 @@ find the contiguous subarray
 (containing at least one number)
 which has the largest sum and return its sum.
 
-
 Example 1:
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
@@ -21,36 +20,7 @@ https://youtu.be/5WZl3MMT0Eg
 """
 
 
-def subarraySum(nums, k):
-    subarrayFound = 0
-
-    if (len(nums) == 1):
-        if nums[0] == k:
-            return 1
-        else:
-            return 0
-
-    else:
-        subarraySum = 0
-        subarray_index_start = 0
-
-        for i in range(0, len(nums), 1):
-
-            # currentEl = nums[i]
-
-            subarraySum += nums[i]
-            print(subarraySum)
-
-            while (subarraySum > k):
-
-                subarraySum -= nums[subarray_index_start]
-                subarray_index_start += 1
-            
-            if subarraySum == k:
-                print("sum", subarraySum, "index", i, "start", subarray_index_start)
-                subarrayFound +=1
-
-        return subarrayFound
+def subarraySum(nums):
 
 if __name__ == "__main__":
     result = subarraySum([-1, -1, 1], 0)
