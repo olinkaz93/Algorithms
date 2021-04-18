b9425d196cd53200e4147600b2de92c2477b3525
Ola Zieminska <oziemins@cisco.com>
2021-04-18 18:58:45 +0200

    560_leet

--- Interview_Examples/Leetcode/560_SubarraySumEqualsK.py
+++ Interview_Examples/Leetcode/560_SubarraySumEqualsK.py
@@ -1,10 +1,20 @@
 "SLINDING WINDOW ONLY FOR POSITIVE NUMBERS, NEGATIVE CANNOT !"
 # https://www.youtube.com/watch?v=pNqL1OX7CSg
+
+"""
+
+
+"""
+
+
 def subarraySum(nums, k):
     subarrayFound = 0
 
-    if (len(nums) == 0):
-        return subarrayFound
+    if (len(nums) == 1):
+        if nums[0] == k:
+            return 1
+        else:
+            return 0
 
     else:
         subarraySum = 0
@@ -15,12 +25,14 @@ def subarraySum(nums, k):
             # currentEl = nums[i]
 
             subarraySum += nums[i]
+            print(subarraySum)
 
             while (subarraySum > k):
                 subarraySum -= nums[subarray_index_start]
                 subarray_index_start += 1
 
             if subarraySum == k:
+                print("sum", subarraySum, "index", i, "start", subarray_index_start)
                 subarrayFound += 1
 
         return subarrayFound
